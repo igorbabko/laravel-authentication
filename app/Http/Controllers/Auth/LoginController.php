35404d519c263e16e4b59852e3dc65b585ad4726
Igor Babko <i.i.babko@gmail.com>
2022-11-06 12:57:37 +0200

    wip

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     public function create()
     {
         return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (! Auth::attempt($request->only('email', 'password'))) {
+            return back()
+                ->withInput()
+                ->withErrors([
+                    'email' => 'These credentials do not match our records.'
+                ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,17 +38,28 @@
                 </svg>
                 <h3 class="text-sm font-medium text-red-800">These credentials do not match our records.</h3>
             </div>
-            <form action="#" autocomplete="off" class="mt-6 space-y-6">
+            <form action="{{ route('login') }}" method="POST" autocomplete="off" novalidate class="space-y-6">
+                @csrf
                 <div>
                     <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <div class="relative mt-1 rounded-md shadow-sm">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-                            <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <svg class="{{ $errors->has('email') ? 'text-red-400' : 'text-gray-400' }} h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                             </svg>
                         </div>
-                        <input type="email" id="email" name="email" class="w-full rounded-md border-gray-300 pl-10 text-sm focus:border-green-500 focus:ring-green-500" placeholder="john@example.com" />
+                        <input type="email" id="email" name="email" value="{{ old('email') }}" class="{{ $errors->has('email') ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-500 focus:ring-red-500' : 'border-gray-300 text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500' }} w-full rounded-md pl-10 pr-10 text-sm" placeholder="john@example.com" />
+                        @if ($errors->has('email'))
+                            <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+                                <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        @endif
                     </div>
+                    @if ($errors->has('email'))
+                        <p class="mt-2 text-sm text-red-600">{{ $errors->first('email') }}</p>
+                    @endif
                 </div>
 
                 <div>
