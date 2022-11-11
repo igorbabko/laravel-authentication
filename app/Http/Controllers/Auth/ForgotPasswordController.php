18259932ca1c5611e7bdf18447a231bb611ca555
Igor Babko <i.i.babko@gmail.com>
2022-11-11 12:00:32 +0200

    12. Forgot password logic

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -17,5 +18,16 @@ class ForgotPasswordController extends Controller
         $request->validate([
             'email' => ['required', 'email'],
         ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status === Password::RESET_LINK_SENT) {
+            return back()->with('status', trans($status));
+        }
+
+        return back()->withInput($request->only('email'))
+                     ->withErrors(['email' => trans($status)]);
     }
 }
